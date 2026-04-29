1965197fc1a0b3779d04114af5a4e3d60db83f0e
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-04-29 11:45:42 +0100

    Add files via upload

---
+++ modulo.py
@@ -0,0 +1,10 @@
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+def main():
+    print(soma(10, 20))
+    print(soma(20, 30))
+# print(__name__)
+if __name__ == "__main__":
+    main()
